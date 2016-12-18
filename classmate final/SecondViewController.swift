510f42081c2cbe08acc736d11784515b3519c43e
Jalaj Kothari <jalajkothari@macbook-air-2.local>
2016-12-18 07:54:45 -0800

    Initial Commit

---
+++ classmate final/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  classmate final
+//
+//  Created by Jalaj Kothari on 12/18/16.
+//  Copyright © 2016 Sushanth. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
